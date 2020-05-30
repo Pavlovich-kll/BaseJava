518bd113d558c6ca26cb1b9c8f1d648afd9fd336
Pavlovich-kll <Pavlovich.kll@users.noreply.github.com>
2020-05-30 23:51:02 +0300

    fixed fields to "private" and removed method "getInd"

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -4,22 +4,21 @@ import java.util.Arrays;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
-    int size;
-    int ind;
+    private Resume[] storage = new Resume[10000];
+    private int size;
 
-    void clear() {
+    public void clear() {
         Arrays.fill(storage, 0, size, null);
         size = 0;
     }
 
-    void save(Resume r) {
+    public void save(Resume r) {
         storage[size] = r;
         size++;
     }
 
-    Resume get(String uuid) {
-        for (ind = 0; ind < size; ind++) {
+    public Resume get(String uuid) {
+        for (int ind = 0; ind < size; ind++) {
             if (storage[ind].uuid.equals(uuid)) {
                 return storage[ind];
             }
@@ -27,25 +26,25 @@ public class ArrayStorage {
         return null;
     }
 
-    void delete(String uuid) {
-        size--;
-        storage[getInd(uuid)] = storage[size];
-        storage[size] = null;
+    public void delete(String uuid) {
+        int indGet = Arrays.asList(storage).indexOf(get(uuid));
+        if (indGet >= 0) {
+            size--;
+            storage[indGet] = storage[size];
+            storage[size] = null;
+        } else {
+            System.out.println("Резюме отсутсвует, удаление невозможно");
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
+    public Resume[] getAll() {
         return Arrays.copyOf(storage, size);
     }
 
-    int size() {
+    public int size() {
         return size;
-    }
-
-    int getInd(String uuid) {
-        get(uuid);
-        return ind;
     }
 }
