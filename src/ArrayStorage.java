8d0e00ace1f659f3b06544c90298887646074b04
Pavlovich-kll <Pavlovich.kll@users.noreply.github.com>
2020-05-30 16:59:44 +0300

    first try

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,51 @@
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int size;
+    int ind;
 
     void clear() {
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
     }
 
     void save(Resume r) {
+        storage[size] = r;
+        size++;
     }
 
     Resume get(String uuid) {
+        for (ind = 0; ind < size; ind++) {
+            if (storage[ind].uuid.equals(uuid)) {
+                return storage[ind];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        size--;
+        storage[getInd(uuid)] = storage[size];
+        storage[size] = null;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return Arrays.copyOf(storage, size);
     }
 
     int size() {
-        return 0;
+        return size;
+    }
+
+    int getInd(String uuid) {
+        get(uuid);
+        return ind;
     }
 }
