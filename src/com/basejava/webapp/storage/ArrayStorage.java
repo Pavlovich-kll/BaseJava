12e392350cb12ee829747e723641be540754ce5c
Pavlovich-kll <Pavlovich.kll@users.noreply.github.com>
2020-06-03 15:58:06 +0300

    fixed method name, removed empty line, private method moved in the end

--- src/com/basejava/webapp/storage/ArrayStorage.java
+++ src/com/basejava/webapp/storage/ArrayStorage.java
@@ -9,10 +9,10 @@ public class ArrayStorage {
     private int size;
 
     public void save(Resume r) {
-        int index = getInd(r.getUuid());
+        int index = getIndex(r.getUuid());
         if (index >= 0) {
             System.out.println("ERROR: Резюме " + r.getUuid() + "  уже существует");
-        } else if(index >= size) {
+        } else if (storage.length <= size) {
             System.out.println("ERROR: Количество резюме превысило допустимое значение");
         } else {
             storage[size] = r;
@@ -21,7 +21,7 @@ public class ArrayStorage {
     }
 
     public void update(Resume r) {
-        int index = getInd(r.getUuid());
+        int index = getIndex(r.getUuid());
         if (index >= 0) {
             storage[index] = r;
         } else {
@@ -30,24 +30,16 @@ public class ArrayStorage {
     }
 
     public Resume get(String uuid) {
-        int index = getInd(uuid);
+        int index = getIndex(uuid);
         if (index >= 0) {
             return storage[index];
         }
+        System.out.println(("ERROR: Резюме " + uuid + " не существует"));
         return null;
     }
 
-    private int getInd(String uuid) {
-        for (int i = 0; i < size; i++) {
-            if (storage[i].getUuid().equals(uuid)) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
     public void delete(String uuid) {
-        int index = getInd(uuid);
+        int index = getIndex(uuid);
         if (index >= 0) {
             storage[index] = storage[size - 1];
             storage[size - 1] = null;
@@ -56,16 +48,26 @@ public class ArrayStorage {
             System.out.println("ERROR: Резюме " + uuid + " отсутсвует, удаление невозможно");
         }
     }
+
     public void clear() {
         Arrays.fill(storage, 0, size, null);
         size = 0;
     }
 
     public Resume[] getAll() {
-        return Arrays.copyOf(storage, size);
+        return Arrays.copyOfRange(storage, 0, size);
     }
 
     public int size() {
         return size;
+    }
+
+    private int getIndex(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].getUuid().equals(uuid)) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
