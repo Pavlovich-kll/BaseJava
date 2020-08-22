c7fdecea574c12d9edd998923d8c950669db8193
Pavlovich-kll <Pavlovich.kll@users.noreply.github.com>
2020-08-22 11:59:27 +0300

    HW17: Deployment with the Heroku CLI

--- src/com/basejava/webapp/Config.java
+++ src/com/basejava/webapp/Config.java
@@ -4,14 +4,13 @@ import com.basejava.webapp.storage.SqlStorage;
 import com.basejava.webapp.storage.Storage;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 public class Config {
     private static final Config INSTANCE = new Config();
-    protected final File PROPS = new File("C:\\Users\\Kirill\\Documents\\GitHub\\basejava\\basejava\\config\\resumes.properties");
+    protected final String PROPS ="/resumes.properties";
 
     private final File storageDir;
     private final Storage storage;
@@ -21,13 +20,13 @@ public class Config {
     }
 
     private Config() {
-        try (InputStream is = new FileInputStream(PROPS)) {
+        try (InputStream is = Config.class.getResourceAsStream(PROPS)) {
             Properties properties = new Properties();
             properties.load(is);//выявляем ошибки на старте приложения
             storageDir = new File(properties.getProperty("storage.dir"));
             storage = new SqlStorage(properties.getProperty("db.url"), properties.getProperty("db.user"), properties.getProperty("db.password"));
         } catch (IOException e) {
-            throw new IllegalStateException("Invalid config file " + PROPS.getAbsolutePath());
+            throw new IllegalStateException("Invalid config file " + PROPS);
         }
     }
 
